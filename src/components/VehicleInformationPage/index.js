3972e75ec0f3703ef68ea2b3502af965da73a7a5
Purvi <47726963+pandyapurvi@users.noreply.github.com>
2021-04-29 12:28:37 +1000

    Update index.js

--- src/components/VehicleInformationPage/index.js
+++ src/components/VehicleInformationPage/index.js
@@ -5,30 +5,20 @@ import { AppContext } from '../../AppContext';
 import StateHadler from '../../StateHandler';
 
 const VehicleInformation = (props) => {
-  
-  // const [detail, setDetail] = useContext(AppContext);
   const history = useHistory();
-  const {  dataChange, onChangeData, detailChange, onChangeDetail, data} = StateHadler()
-console.log('data in 2nd page')
-  // if (loading) {
-  //   return <p>Data is loading...</p>;
-  // }
-
-  // if (error) {
-  //   return <p>There was an error loading your data!</p>;
-  // }
-
-      
-  
-  // const onSubmit = (e) => {
-  //   e.preventdefault()
-  // }
+  const {data} = StateHadler()
+  console.log('data in 2nd page')
+ 
 
   return (
     <form >
       <h2>Vehicle Information Page</h2>
       {console.log('@@@@@', data)} 
-      {JSON.stringify(data.vehicle)}
+      <p>{data.plate_number}</p>
+      {JSON.stringify((data.vehicle.vin).replace(/.(?=.{4,}$)/g, '*'))}
+      <p>{JSON.stringify(data.insurer)}</p>
+      <p>{JSON.stringify(data.registration)}</p>
+//       {JSON.stringify(data.vehicle)}
       {/* {setData(data)} */}
       </form>
   )
